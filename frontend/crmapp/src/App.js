14b37f2bee2641a9340d4c1dd78ba1f6a98c3707
anja <anja394e@gmail.com>
2023-10-09 12:09:46 +0300

    initial commit

---
+++ frontend/crmapp/src/App.js
@@ -0,0 +1,18 @@
+// src/App.js
+import React from 'react';
+import CustomerList from './components/CustomerList';
+
+
+
+function App() {
+  return (
+      <div>
+        <h1>React Spring Boot App</h1>
+        <CustomerList />
+      </div>
+  );
+}
+
+
+
+export default App;
